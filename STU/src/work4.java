5126ced0ba2abf28f818e0b424ab349c1a5d1339
ViChen9 <1140417534@qq.com>
2020-11-02 22:14:37 +0800

    OJBK

--- STU/src/work4.java
+++ STU/src/work4.java
@@ -1,39 +1,24 @@
-import java.util.Arrays;
-import java.util.Scanner;
-
 /**
  * 4. 编程题 自定义数组扩容规则，当已存储元素数量达到总容量的 80%时，扩容 1.5 倍。 例如，总容量是 10，当输入第 8 个元素时，数组进行扩容，容量从 10 变 15。
  */
+import java.util.Arrays;
+
 public class work4 {
     public static void main(String[] args) {
-        int[] arr1 = new int[10];
-        Scanner sc =new Scanner(System.in);
-        for (int i = 0; i < arr1.length; i++) {
-            arr1[i] = i + 1;
-            if (i>=arr1.length*0.8){
-                System.out.print("存储空间已不足20%，是否扩容空间 Y/N");
-                if (sc.next()=="y"){
-                    System.out.print("扩容");
-                }if(sc.next()=="n"){
-                    System.out.print("");
-                    //break;
-                }
+        int[] arr = new int[10];
+        int i = 0;
+        do {
+            if ((i + 1) * 1.0 / arr.length > 0.8) {
+                System.out.println("存储元素数量已达到总容量80%，扩容 1.5 倍");
+                    int[] newarr = new int[(int) (arr.length * 1.5)];
+                    for (int j = 0; j < arr.length; j++) {
+                        newarr[j] = arr[j];
+                    }
+                    arr = newarr;
             }
-        }
-
-
-        System.out.println(Arrays.toString(arr1));
-        /**int s = (int) (arr1.length * 0.8);
-         int[] arr2 = new int[arr1.length + (arr1.length >> 1)];
-         for (int i = 0; i <= s; i++) {
-         arr1[i] = i + 1;
-         if (i == s) {
-         }
-         }
-         for (int a = 0; a < arr1.length; a++) {
-         arr2[a] = arr1[a];
-         }
-         arr1=arr2;
-         System.out.println(Arrays.toString(arr1));**/
+            arr[i] = i + 1;
+            System.out.printf("添加第%d个元素，当前数组长度为%d:%s\n", i + 1, arr.length, Arrays.toString(arr));
+            i += 1;
+        } while (i < 26);
     }
 }
--- STU/src/work5.java
+++ STU/src/work5.java
@@ -3,7 +3,28 @@
  */
 public class work5 {
     public static void main(String[] args) {
-        System.out.println("fuck off!");
-        System.out.println("hello word");
+        // 定义二维数组
+        String[][] arr2 = new String[17][17];
+        // 行
+        for (int i = 0; i < arr2.length; i++) {
+            // 列
+            for (int j = 0; j < arr2[0].length; j++) {
+                if (i == 0) {
+                    arr2[i][j] = String.format("%x", j - 1);
+                } else if (j == 0) {
+                    arr2[i][j] = String.format("%x", i - 1);
+                } else {
+                    arr2[i][j] = "+";
+                }
+            }
+        }
+        // 修改第一个元素为空格
+        arr2[0][0] = " ";
+        for (int i = 0; i < arr2.length; i++) {
+            for (int j = 0; j < arr2[i].length; j++) {
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
