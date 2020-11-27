133b08ff8abfc930f1779c9f7c88b8bd2eecc353
ViChen9 <1140417534@qq.com>
2020-11-27 17:41:55 +0800

    OK

--- STU/Module2/Tack3/TeacherTest.java
+++ STU/Module2/Tack3/TeacherTest.java
@@ -11,7 +11,7 @@ public class TeacherTest {
     public static void main(String[] args) {
 
         // 1.使用无参方式构造Teacher类型的兑现并打印特征
-        Teacher t1 = new Teacher();
+        Teacher t1 = new Teacher( );
         t1.show();
 
     }
